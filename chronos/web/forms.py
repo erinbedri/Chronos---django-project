e30e1ad900c1da1c364a8063bc03de89cd4f6418
erinbedri <erinbedri@gmail.com>
2022-10-03 18:15:19 +0200

    added classes to forms

--- chronos/web/forms.py
+++ chronos/web/forms.py
@@ -25,6 +25,55 @@ class CreateWatchForm(forms.ModelForm):
     class Meta:
         model = Watch
         fields = ('owner', 'brand', 'model', 'reference_number', 'year', 'style', 'condition', 'description', 'image')
+        widgets = {
+            'owner': forms.Select(
+                attrs={
+                    'class': 'form-control',
+                }
+            ),
+            'brand': forms.TextInput(
+                attrs={
+                    'class': 'form-control'
+                }
+            ),
+            'model': forms.TextInput(
+                attrs={
+                    'class': 'form-control'
+                }
+            ),
+            'reference_number': forms.TextInput(
+                attrs={
+                    'class': 'form-control'
+                }
+            ),
+            'year': forms.NumberInput(
+                attrs={
+                    'class': 'form-control'
+                }
+            ),
+            'style': forms.Select(
+                attrs={
+                    'class': 'form-control'
+                }
+            ),
+            'condition': forms.Textarea(
+                attrs={
+                    'class': 'form-control',
+                    'rows': 1
+                }
+            ),
+            'description': forms.Textarea(
+                attrs={
+                    'class': 'form-control',
+                    'rows': 2
+                }
+            ),
+            'image': forms.FileInput(
+                attrs={
+                    'class': 'form-control',
+                }
+            )
+        }
 
 
 class DeleteWatchForm(forms.ModelForm):
@@ -37,4 +86,52 @@ class EditWatchForm(forms.ModelForm):
     class Meta:
         model = Watch
         fields = ('owner', 'brand', 'model', 'reference_number', 'year', 'style', 'condition', 'description', 'image')
-
+        widgets = {
+            'owner': forms.Select(
+                attrs={
+                    'class': 'form-control',
+                }
+            ),
+            'brand': forms.TextInput(
+                attrs={
+                    'class': 'form-control'
+                }
+            ),
+            'model': forms.TextInput(
+                attrs={
+                    'class': 'form-control'
+                }
+            ),
+            'reference_number': forms.TextInput(
+                attrs={
+                    'class': 'form-control'
+                }
+            ),
+            'year': forms.NumberInput(
+                attrs={
+                    'class': 'form-control'
+                }
+            ),
+            'style': forms.Select(
+                attrs={
+                    'class': 'form-control'
+                }
+            ),
+            'condition': forms.Textarea(
+                attrs={
+                    'class': 'form-control',
+                    'rows': 1
+                }
+            ),
+            'description': forms.Textarea(
+                attrs={
+                    'class': 'form-control',
+                    'rows': 2
+                }
+            ),
+            'image': forms.FileInput(
+                attrs={
+                    'class': 'form-control',
+                }
+            )
+        }
