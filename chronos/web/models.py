044ad46b6b155bf60006afc0c0640a7fcd1242d8
erinbedri <erinbedri@gmail.com>
2022-10-04 20:09:17 +0200

    fixed many to one relationship

--- chronos/web/models.py
+++ chronos/web/models.py
@@ -7,64 +7,6 @@ from django.db.models import CASCADE
 
 from chronos.web.validators import file_size
 
-"""
-class Profile(models.Model):
-    USERNAME_MAX_LEN = 30
-
-    FIRST_NAME_MIN_LEN = 2
-    FIRST_NAME_MAX_LEN = 30
-
-    LAST_NAME_MIN_LEN = 2
-    LAST_NAME_MAX_LEN = 30
-
-    username = models.CharField(
-        max_length=USERNAME_MAX_LEN,
-        unique=True
-    )
-
-    email = models.EmailField(
-        blank=True,
-        null=True
-    )
-
-    first_name = models.CharField(
-        max_length=FIRST_NAME_MAX_LEN,
-        blank=True,
-        null=True,
-        validators=[
-            MinLengthValidator(FIRST_NAME_MIN_LEN),
-        ]
-    )
-
-    last_name = models.CharField(
-        max_length=LAST_NAME_MAX_LEN,
-        blank=True,
-        null=True,
-        validators=[
-            MinLengthValidator(LAST_NAME_MIN_LEN),
-        ]
-    )
-
-    image = models.ImageField(
-        upload_to='profile',
-        null=True,
-        blank=True,
-        validators=(
-            file_size,
-        )
-    )
-
-    created_at = models.DateTimeField(
-        auto_now_add=True
-    )
-
-    updated_at = models.DateTimeField(
-        auto_now=True
-    )
-
-    def __str__(self):
-        return f'{self.first_name} {self.last_name}'
-"""
 
 class Watch(models.Model):
     BRAND_MAX_LEN = 30
