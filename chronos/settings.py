d5ab9c8c87c50d31796762a378fce23b521ad929
erinbedri <erinbedri@gmail.com>
2022-10-05 09:26:21 +0200

    installed crispy

--- chronos/settings.py
+++ chronos/settings.py
@@ -16,8 +16,15 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
+    "crispy_forms",
+    "crispy_bootstrap5",
+
     'chronos.web',
 ]
+
+CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap5"
+
+CRISPY_TEMPLATE_PACK = "bootstrap5"
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
