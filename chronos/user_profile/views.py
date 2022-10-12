29faf2bf9a09b78417e6c96a1acad2eed4361b39
erinbedri <erinbedri@gmail.com>
2022-10-12 14:52:00 +0200

    user app messages added

--- chronos/user_profile/views.py
+++ chronos/user_profile/views.py
@@ -6,6 +6,18 @@ from django.shortcuts import render, redirect
 from chronos.user_profile.forms import NewUserForm, PrettyAuthenticationForm, EditProfileForm, DeleteProfileForm
 from chronos.watch.models import Watch
 
+REGISTRATION_SUCCESS_MESSAGE = 'Registration successful!'
+REGISTRATION_ERROR_MESSAGE = 'Unsuccessful registration. Invalid information.'
+
+LOGIN_SUCCESS_MESSAGE = 'You are successfully logged in '
+LOGIN_ERROR_MESSAGE = 'Invalid username or password!'
+
+LOGOUT_SUCCESS_MESSAGE = 'You are successfully logged out.'
+
+PROFILE_EDIT_SUCCESS_MESSAGE = 'You successfully updated your profile information!'
+
+PROFILE_DELETE_SUCCESS_MESSAGE = 'You successfully deleted your profile information!'
+
 
 def register_profile(request):
     if request.method == 'POST':
@@ -13,15 +25,17 @@ def register_profile(request):
         if form.is_valid():
             user = form.save()
             login(request, user)
-            messages.success(request, 'Registration successful!')
+            messages.success(request, REGISTRATION_SUCCESS_MESSAGE)
             return redirect('show dashboard')
-        messages.error(request, 'Unsuccessful registration. Invalid information.')
+        else:
+            messages.error(request, REGISTRATION_ERROR_MESSAGE)
     else:
         form = NewUserForm()
 
     context = {
         'form': form,
     }
+
     return render(request, 'profile/profile_register.html', context)
 
 
@@ -34,24 +48,26 @@ def login_profile(request):
             user = authenticate(username=username, password=password)
             if user is not None:
                 login(request, user)
-                messages.info(request, f'You are now logged in as {username}!')
+                messages.success(request, LOGIN_SUCCESS_MESSAGE + f'as {username}!')
                 return redirect('show dashboard')
             else:
-                messages.error(request, f'Invalid username or password!')
+                messages.error(request, LOGIN_ERROR_MESSAGE)
         else:
-            messages.error(request, 'Invalid username or password!')
+            messages.error(request, LOGIN_ERROR_MESSAGE)
+
     form = PrettyAuthenticationForm()
 
     context = {
         'form': form,
     }
+
     return render(request, 'profile/profile_login.html', context)
 
 
 @login_required
 def logout_profile(request):
     logout(request)
-    messages.info(request, 'You are successfully logged out.')
+    messages.success(request, LOGOUT_SUCCESS_MESSAGE)
     return redirect('show homepage')
 
 
@@ -62,6 +78,7 @@ def show_profile(request):
     context = {
         'watch_count': watch_count,
     }
+
     return render(request, 'profile/profile_details.html', context)
 
 
@@ -71,6 +88,7 @@ def edit_profile(request):
         form = EditProfileForm(request.POST, request.FILES, instance=request.user)
         if form.is_valid():
             form.save()
+            messages.success(request, PROFILE_EDIT_SUCCESS_MESSAGE)
             return redirect('show profile')
     else:
         form = EditProfileForm(instance=request.user)
@@ -78,6 +96,7 @@ def edit_profile(request):
     context = {
         'form': form
     }
+
     return render(request, 'profile/profile_edit.html', context)
 
 
@@ -87,6 +106,7 @@ def delete_profile(request):
         form = DeleteProfileForm(request.POST, request.FILES, instance=request.user)
         if form.is_valid():
             request.user.delete()
+            messages.success(request, PROFILE_DELETE_SUCCESS_MESSAGE)
             return redirect('show homepage')
     else:
         form = DeleteProfileForm(instance=request.user)
@@ -94,4 +114,5 @@ def delete_profile(request):
     context = {
         'form': form
     }
+
     return render(request, 'profile/profile_delete.html', context)
